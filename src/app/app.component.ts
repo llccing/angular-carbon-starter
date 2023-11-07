2d8a6915cfbc97a8ffd60d0f7a81442224733f94
Chunfeng Liu <cliu3@rocketsoftware.com>
2023-11-07 11:04:21 +0800

    feat: convert to a standalone project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-v17';
